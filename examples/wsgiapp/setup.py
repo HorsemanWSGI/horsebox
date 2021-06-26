78b46a6e1ad3788d8c4aba6f2ec2104e5f0d1e9e
Souheil Chelfouh <trollfot@gmail.com>
2021-06-26 20:20:06 +0200

    Added very barebone module/wsgiapp example

---
+++ examples/wsgiapp/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wsgiapp',
+    version='0.1',
+    description='Horsebox deployment demo',
+    author='Souheil Chelfouh',
+    packages=['wsgiapp'],
+    zip_safe=True
+)
