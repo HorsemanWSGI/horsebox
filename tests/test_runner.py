0d8895435be3f8c447a7dc06fc5d367cb8c62dcd
Souheil Chelfouh <trollfot@gmail.com>
2021-06-26 23:42:36 +0200

    Added runner tests

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -1,5 +1,5 @@
 import pytest
-from horsebox.runner import get_project
+from horsebox.runner import get_project, main
 from horsebox.types import Project
 from horsebox.project import DefaultProject
 
@@ -27,6 +27,17 @@ class MyProject(Project):
     def from_config(cls, config):
         cls.check_config(config)
         return cls(config['name'])
+
+
+@pytest.fixture(scope="session")
+def config_file(tmpdir_factory):
+    fn = tmpdir_factory.mktemp("conf").join("config.yaml")
+    with fn.open('w+') as fd:
+        fd.write(
+            "runner: !name:print\n"
+            "  - I am working"
+        )
+    return fn
 
 
 class TestProjectExtraction:
@@ -62,3 +73,28 @@ class TestProjectExtraction:
         assert str(exc.value) == (
             "'Expecting key `name` with a str type value.'"
         )
+
+
+class TestRunner:
+
+    def test_wrong_config_path(self):
+        from minicli import run
+
+        with pytest.raises(FileNotFoundError):
+            run('run', 'toto')
+
+    def test_config_path(self, config_file):
+        from minicli import run
+        run('run', str(config_file))
+
+    def test_wrong_logging_level(self, capsys, config_file):
+        from minicli import run
+
+        with pytest.raises(SystemExit) as exc:
+            run('run', str(config_file), '-l', 'something')
+
+        out, err = capsys.readouterr()
+        assert (
+            "invalid choice: 'something' (choose from "
+            "'critical', 'error', 'warning', 'info', 'debug')"
+        ) in err
